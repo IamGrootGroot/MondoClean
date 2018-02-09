95c56267133d03467104cb8e818b60f08a6b1912
IamGrootGroot <maxencepelloux95@gmail.com>
2018-02-09 21:26:16 +0100

    Hotfixes

--- data_Cleaner_Module/data_Cleaner.py
+++ data_Cleaner_Module/data_Cleaner.py
@@ -11,6 +11,7 @@ class Cleaner:
         self.sheetN = 0
         self.banned = ['.',' ','#N/A','#DIV/0','inconnu','?','NA','None']
         self.path = path
+        self.dateStyle = openpyxl.styles.NamedStyle(name="dateStyle", number_format='DD/MM/YYYY')
 
     def openWB(self, key, path):
         """ Load and open cleaner workbook:
@@ -21,10 +22,13 @@ class Cleaner:
         mods = {'basic': 1, 'aggreg': 2, 'joint': 3}
         if key == 1:
             self.wb = openpyxl.load_workbook(self.path)
+            self.wb.add_named_style(self.dateStyle)
         if key == 2:
             self.wb1 = openpyxl.load_workbook(path)
+            self.wb.add_named_style(self.dateStyle)
         if key == 3:
             self.wb2 = openpyxl.load_workbook(path)
+            self.wb.add_named_style(self.dateStyle)
 
     def anonymize(self, colIndexAN):
         """ Anonymization function: Sets cell values of given columns to their respective row index value
@@ -42,7 +46,7 @@ class Cleaner:
         sheet = self.wb.worksheets[self.sheetN]
         seen = {}
         if type(colIndexAN) is not list:
-            for i in range(2, sheet.max_row):
+            for i in range(2, sheet.max_row+1):
                 self.wba.active.cell(row=i-1, column=2).value = sheet.cell(row=i, column=colIndexAN).value
                 if sheet.cell(row=i, column=colIndexAN).value not in seen.values():
                     seen.update({i-track:sheet.cell(row=i, column=colIndexAN).value})
@@ -52,10 +56,10 @@ class Cleaner:
                     track = track+1
                     self.wba.active.cell(row=i-1, column=1).value = list(seen.keys())[list(seen.values()).index(sheet.cell(row=i, column=colIndexAN).value)]
                     sheet.cell(row=i, column=colIndexAN).value = list(seen.keys())[list(seen.values()).index(sheet.cell(row=i, column=colIndexAN).value)]
-            colHead = {cell.value for n, cell in enumerate(list(sheet.rows)[0]) if n == colIndexAN}
+            colHead = {cell.value for n, cell in enumerate(list(sheet.rows)[0]) if n+1 == colIndexAN}
             print('Succesfully anonymized column', colHead,'.')
         else:
-            for i in range(2, sheet.max_row):
+            for i in range(2, sheet.max_row+1):
                 sequence = ''
                 for k in colIndexAN:
                     sequence += str(sheet.cell(row=i, column=k).value)
@@ -82,11 +86,10 @@ class Cleaner:
         try :
             sheet = self.wb.worksheets[self.sheetN]
             track = 0;
-            for i in range(1, sheet.max_column) :
-                for j in range(1, sheet.max_row) :
-                    for ban in self.banned :
-                        if sheet.cell(row=j, column=i).value == ban:
-                            sheet.cell(row=j, column=i).value = ''
+            for i in range(1, sheet.max_column+1) :
+                for j in range(2, sheet.max_row+1) :
+                    if sheet.cell(row=j, column=i).value in self.banned:
+                            sheet.cell(row=j, column=i).value = None
                             track=track+1
                     if ("." in str(sheet.cell(row=j, column=i).value)) or ("," in str(sheet.cell(row=j, column=i).value)):
                         track=track+1
@@ -102,7 +105,7 @@ class Cleaner:
             for n, head in enumerate(list(sheet.rows)[0]):
                 dateKey = ["date", "Date", "DATE", "DT "]
                 if any(key in head.value for key in dateKey):
-                    for k in range(2, sheet.max_row):
+                    for k in range(2, sheet.max_row+1):
                         if type(sheet.cell(row=k, column=n+1).value) is not datetime:
                             if str(sheet.cell(row=k, column=n+1).value) != '' and 'None' not in str(sheet.cell(row=k, column=n+1).value):
                                 if ' ' in str(sheet.cell(row=k, column=n+1).value):
@@ -110,29 +113,31 @@ class Cleaner:
                                 if '.' in str(sheet.cell(row=k, column=n+1).value):
                                     sheet.cell(row=k, column=n+1).value = str(sheet.cell(row=k, column=n+1).value).replace('.','')
                                 dateObject = datetime.strptime(str(sheet.cell(row=k, column=n+1).value),formatIn)
-                                sheet.cell(row=k, column=n+1).value = dateObject.strftime('%d/%m/%Y')
+                                sheet.cell(row=k, column=n+1).value = dateObject.strptime(dateObject.strftime('%d/%m/%Y'),'%d/%m/%Y')
+                                sheet.cell(row=k, column=n+1).style = "dateStyle"
                         else:
-                            sheet.cell(row=k, column=n+1).value = sheet.cell(row=k, column=n+1).value.strftime('%d/%m/%Y')
+                            sheet.cell(row=k, column=n+1).value = sheet.cell(row=k, column=n+1).value.strptime(sheet.cell(row=k, column=n+1).value.strftime('%d/%m/%Y'),'%d/%m/%Y')
+                            sheet.cell(row=k, column=n+1).style = "dateStyle"
             print('Dates reformatted.')
         except ValueError:
-            print('Error at cell[',k,' ',n,'], invalid cell content: ',str(sheet.cell(row=k, column=n+1).value),"""please make sure the formatIn
+            print('Error at cell[',k,' ',n+1,'], invalid cell content: ', str(sheet.cell(row=k, column=n+1).value),"""please make sure the formatIn
             ... represents the actual input date format.""")
 
     def aggreg(self, paths):
         """Aggregation function, appends different xlsx files @paths, no need to worry about empty or missing columns.
         WARNING: Column names have to be the same as the ones in the main workbook"""
+        sheet = self.wb.worksheets[self.sheetN]
+        headers = list(sheet.rows)[0]
         for i, p in enumerate(paths):
             wbb = openpyxl.load_workbook(p)
             sheetB = wbb.active
             headersB = list(sheetB.rows)[0]
-            sheet = self.wb.worksheets[self.sheetN]
-            headers = list(sheet.rows)[0]
             patch_row = sheet.max_row
             for n, head in enumerate(headers):
                 for k, headB in enumerate(headersB):
                     if headB.value == head.value:
-                        for j in range(1, sheetB.max_row):
-                            sheet.cell(row=patch_row+j, column=n+1).value = sheetB.cell(row=j+1, column=k+1).value
+                        for j in range(2, sheetB.max_row+1):
+                            sheet.cell(row=patch_row+j-1, column=n+1).value = sheetB.cell(row=j, column=k+1).value
 
     def param(self, listBans):
         """Add new banned characters"""
@@ -165,7 +170,7 @@ class Cleaner:
                 else:
                     dupes.append(cell.row)
             for i in dupes:
-                for j in range(1, sheet.max_column):
+                for j in range(1, sheet.max_column+1):
                     cell(row=i, column=j).fill = PatternFill(bgColor="FFC7CE", fill_type = "solid")
         except:
             print("Can't find duplicates.")
@@ -209,11 +214,11 @@ class Cleaner:
                     pass
                 else:
                     idx.update({i:colj[j]})
-        with progressbar.ProgressBar(max_value=len(idx), widgets=["Adding matched values:", progressbar.Percentage(), progressbar.Bar()]) as bar:
+        with progressbar.ProgressBar(max_value=len(idx), widgets=["Adding matched data:", progressbar.Percentage(), progressbar.Bar()]) as bar:
             for i, j in enumerate(idx.keys()):
                 bar.update(i)
                 for k in range(len(colJoints)):
-                    sheet.cell(row=j+2, column=k+2+mc).value = idx.get(j)[k]
+                    sheet.cell(row=j+2, column=k+1+mc).value = idx.get(j)[k]
         self.purify()
 
     def saveWB(self, key, newPath):
--- test.py
+++ test.py
@@ -1,22 +1,22 @@
 import sys;
 from data_Cleaner_Module import data_Cleaner as DC
 
-path = '/Users/maxencepelloux/Documents/PFE/PFE_Data/Dirty_Data/Book1.xlsx'
-paths = ['/Users/maxencepelloux/Documents/PFE/PFE_Data/Dirty_Data/Book2.xlsx']
-pathJ = '/Users/maxencepelloux/Documents/PFE/PFE_Data/Dirty_Data/Gastro.xlsx'
-newPath = '/Users/maxencepelloux/Documents/PFE/PFE_Data/Clean_Data/GROSTEST.xlsx'
-colComp1 = 6
-colComp2 = 1
-colJoints = [2]
+path = '/Users/maxencepelloux/Documents/PFE/PFE_Data/Dirty_Data/Fichier source étude durée AT VF TIS.xlsx'
+#paths = ['/Users/maxencepelloux/Documents/PFE/PFE_Data/Dirty_Data/Book2.xlsx']
+#pathJ = '/Users/maxencepelloux/Documents/PFE/PFE_Data/Dirty_Data/Gastro.xlsx'
+newPath = '/Users/maxencepelloux/Documents/PFE/PFE_Data/Clean_Data/lol.xlsx'
+#colComp1 = 6
+#colComp2 = 1
+#colJoints = [2]
 formatIn = '%Y%m%d'
 colIndexAN = 1
-colIndexCat = None
+#colIndexCat = None
 cleaner = DC.Cleaner(path)
 cleaner.openWB(1, None)
-cleaner.aggreg(paths)
-cleaner.purify()
+#cleaner.aggreg(paths)
+#cleaner.purify()
 cleaner.changeDate(formatIn)
-cleaner.joint(pathJ, colComp1, colComp2, colJoints)
-cleaner.anonymize(colIndexAN)
+#cleaner.joint(pathJ, colComp1, colComp2, colJoints)
+#cleaner.anonymize(colIndexAN)
 cleaner.purify()
-cleaner.saveWB(2, newPath)
+cleaner.saveWB(1, newPath)
